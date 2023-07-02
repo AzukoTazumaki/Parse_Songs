bed5f5d3319bbc1e3d9fcfcd2271cfbaeaba5ce3
Azuko <tazumaki@mail.ru>
2023-07-02 17:39:12 +0300

    added search for artists

--- parse.py
+++ parse.py
@@ -5,57 +5,64 @@ import requests
 class Songs:
     def __init__(self, name: str):
         self.url: str = 'https://songtranslate.ru/'
-        self.name = name
+        self.name: str = name
 
-    def popular_songs(self):
+    def popular_songs(self) -> tuple:
         page = bs(requests.get(self.url).content, 'html.parser')
-        popular_songs = [
-            x.text.replace('\n', '') for x in
+        popular_songs: list = [
+            tag.text.replace('\n', '') for tag in
             page.find('div', {'class': 'cols'})
             .find_all('div', {'class': 'col'})[0].find('ul', {'class': 'song-list'})
             .find_all('li')
         ]
-        new_songs = [
-            x.text.replace('\n', '') for x in
+        new_songs: list = [
+            tag.text.replace('\n', '') for tag in
             page.find('div', {'class': 'cols'})
             .find_all('div', {'class': 'col'})[1].find('ul', {'class': 'song-list'})
             .find_all('li')
         ]
-        top_songs = [
-            x.text.replace('\n', '').replace('0', '') for x in
+        top_songs: list = [
+            tag.text.replace('\n', '').replace('0', '') for tag in
             page.find('div', {'class': 'cols'})
             .find_all('div', {'class': 'col'})[2].find('ul', {'class': 'song-list'})
             .find_all('li')
         ]
         return popular_songs, new_songs, top_songs
 
-    def search_songs(self):
-        checked_name = self.check_name(self.name)
+    def search_hrefs(self):
+        checked_name: str = self.check_name(self.name)
         search: str = f'/search?q={checked_name}'
-        search_hrefs = [
-            x['href'] for x in bs(requests.get(self.url + search).content, 'html.parser')
+        search_hrefs: list = [
+            link['href'] for link in bs(requests.get(self.url + search).content, 'html.parser')
             .find('div', {'class': 'content'})
             .find_all('a')
-            if '/songs' in x['href']
+            if ('/songs' in link['href']) and ('.html' not in link['href']) and ('_and_' not in link['href'])
         ]
-        artists = self.search_names(search_hrefs)
+        return search_hrefs
+
+    def search_artists(self) -> dict:
+        search_hrefs: list = self.search_hrefs()
+        bio: list = [
+            bs(requests.get(self.url + href).content, 'html.parser')
+            .find('div', {'class': 'bio'}).find('p').text
+            for href in search_hrefs
+        ]
+        artists: list = self.search_names(search_hrefs)
+        return dict(zip(artists, bio))
 
     @staticmethod
     def check_name(name: str) -> str:
-        name_list = ' '.join(x.lower() for x in name.split(' '))
+        name_list: str = ' '.join(x.lower() for x in name.split(' '))
         return name_list
 
     @staticmethod
     def search_names(hrefs: list) -> list:
-        result = []
+        result: list = []
         for href in hrefs:
             if '?' in href:
                 del href
             else:
-                replaced = href.replace('/songs/', '').replace('/', '')
-                name = ' '.join([x.capitalize() for x in replaced.split('_')])
+                replaced: str = href.replace('/songs/', '').replace('/', '')
+                name: str = ' '.join([x.capitalize() for x in replaced.split('_')])
                 result.append(name)
         return result
-
-
-Songs('Lil UZI').popular_songs()
--- project.py
+++ project.py
@@ -9,10 +9,16 @@ def index():
     popular_songs = Songs('').popular_songs()[0]
     new_songs = Songs('').popular_songs()[1]
     top_songs = Songs('').popular_songs()[2]
-    return render_template('index.html',
-                           popular_songs=popular_songs,
-                           new_songs=new_songs,
-                           top_songs=top_songs)
+    return render_template('content.html', popular_songs=popular_songs, new_songs=new_songs, top_songs=top_songs)
+
+
+@project.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'GET':
+        form_data = request.args.getlist('search')[0]
+        dict_data = Songs(form_data).search_artists()
+        return render_template('search_artists.html', data=dict_data, request_search=form_data)
+    return index()
 
 
 if __name__ == '__main__':
