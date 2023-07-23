a1eb2f497ffc8daa96a9f7a95865db8c391dd9a7
Azuko <tazumaki@mail.ru>
2023-07-23 23:06:01 +0300

    upd visual

--- draft.py
+++ draft.py
@@ -1,3 +1,12 @@
-f = [1, 2, 3, 4, 5]
-s = ['а', 'б', 'в', 'г', 'д']
-print(zip(f, s))
+# f = [1, 2, 3, 4, 5]
+# s = ['а', 'б', 'в', 'г', 'д']
+# print(zip(f, s))
+from random import randint
+from datetime import date
+
+result = ''
+for i in range(17):
+    if i == 0:
+        continue
+    result += f'("А{randint(100, 300)}"), '
+print(result)
